9ceb60b7874c0e1b3733beb99706ae806bd1184d
gengjingjiao <zhujinzhujin@zhujindeimac.local>
2016-09-01 15:30:31 +0800

    Initial Commit

---
+++ MyTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTest
+//
+//  Created by zhujin zhujin on 16/9/1.
+//  Copyright © 2016年 zhujin zhujin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
